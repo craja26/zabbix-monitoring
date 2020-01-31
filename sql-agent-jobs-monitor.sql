6881245110ec27d21472b52809ade4823acba2b4
Raja <craja26@gmail.com>
2020-01-31 12:12:56 +0100

    Create sql-agent-jobs-monitor.sql

---
+++ sql-agent-jobs-monitor.sql
@@ -0,0 +1,8 @@
+/*
+This process will generate jobs current status as a json output. Based on status value in the output, will get an alert. 
+While I was planning to configure alerts on Zabbix, read few articles on internet, inspired by an article(link below) 
+and modified code as per our requirements.
+https://www.sqlshack.com/reporting-and-alerting-on-job-failure-in-sql-server/
+
+I am storing
+*/
